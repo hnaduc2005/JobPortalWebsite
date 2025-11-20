45de40a707db833f34234a05e17c0f7b2d518685
Bui Trong Suu <btrongsuu25@gmail.com>
2025-11-20 21:38:07 +0700

    Update header, index, and css base/reset

--- modules/candidate/index.php
+++ modules/candidate/index.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php include __DIR__ . '/../../core/templates/header.php'; ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
 
-<body>
-    <h1>Trang Candidate</h1>
-</body>
 
-</html>
+
+
+<?php include __DIR__ . '/../../core/templates/footer.php'; ?>
