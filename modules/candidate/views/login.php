cd83eb974f2d0aa61e0689f43d2ff84bfa8284d9
Duc <duchna6217@ut.edu.vn>
2025-11-29 08:34:14 +0700

    fix

--- modules/candidate/views/login.php
+++ modules/candidate/views/login.php
@@ -198,7 +198,7 @@ $errors = getSessionFlash('errors');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Đăng Nhập</title>
-    <link rel="stylesheet" href="/assets/css/Candidate/login.css">
+    <link rel="stylesheet" href=" <?php echo BASE_URL ?>  /assets/css/Candidate/login.css">
 </head>
 
 <body>
